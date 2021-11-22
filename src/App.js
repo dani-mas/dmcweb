3f2ec4db2687874a8907fd6a879cc6a277c1e34c
dani-mas <danimascarre@gmail.com>
2021-11-22 23:21:46 +0100

    web3 wallet metamask

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import Home from "./PortfolioContainer/Home/Home";
 import Sidebar from "./PortfolioContainer/Sidebar/Sidebar.js";
+import { Web3ReactProvider } from "@web3-react/core";
+import Web3 from "web3";
 
+function getLibrary(provider) {
+  return new Web3(provider);
+}
 function App() {
   return (
-    <div className="App">
-      <Sidebar />
-      <Home />
-    </div>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <div className="App">
+        <Sidebar />
+        <Home />
+      </div>
+    </Web3ReactProvider>
   );
 }
 
--- src/PortfolioContainer/Sidebar/Sidebar.js
+++ src/PortfolioContainer/Sidebar/Sidebar.js
@@ -4,17 +4,49 @@ import "./Sidebar.css";
 import { useState } from "react";
 import { AiOutlineCloseCircle } from "react-icons/ai";
 import { AiOutlineMenu } from "react-icons/ai";
+import { injected } from "../components/wallet/connectors";
+import { useWeb3React } from "@web3-react/core";
 
 function Sidebar() {
+  const { active, account, library, connector, activate, desactivate } =
+    useWeb3React();
+
   const [sidebar, setSidebar] = useState(false);
   const showSidebar = () => setSidebar(!sidebar);
-  const [button, setButton] = useState(false);
-  const showButton = () => setButton(!button);
+
+  async function connect() {
+    try {
+      await activate(injected);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  async function disconnect() {
+    try {
+      desactivate();
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
 
   return (
     <>
+      <header>
+        <div className="navbar-wallet">
+          <AiOutlineMenu className={"toggle"} onClick={showSidebar} />
+          <button onClick={connect}>Connect to MetaMask</button>
+          <button onClick={disconnect}>Disconnect</button>
+          {active ? (
+            <span>
+              Connected with <b>{account}</b>
+            </span>
+          ) : (
+            <span className="msg-wallet">Not connected</span>
+          )}
+        </div>
+      </header>
       <div className={sidebar ? "sidenav active" : "sidenav"}>
-        <AiOutlineMenu className={"toggle"} onClick={showSidebar} />
         <ul>
           <AiOutlineCloseCircle id="close" onClick={showSidebar} />
           <li>
